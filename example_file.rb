2c36694cfcdc7b9ca1dd8d1979243989c89c06c9
Jacob Kagon <jacob@kagon.net>
2020-10-06 01:01:23 +0000

    Automatically backed up by Learn

---
+++ example_file.rb
@@ -0,0 +1,10 @@
+spice_rack = [
+  ["Mace", "Ginger", "Marojam"],          
+  ["Paprika", "Fajita Mix", "Coriander"], 
+  ["Parsley", "Sage", "Rosemary"]         
+]
+
+
+spice_rack[0][0] = "Sugar"
+
+puts spice_rack
